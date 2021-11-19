d9c385ad418965d7dbea8e7204127b5f6964fb0d
dawidjar97 <mr.dj.xd@gmail.com>
2021-11-19 09:31:48 +0100

    lab2 - update

--- Lab2/main.c
+++ Lab2/main.c
@@ -1,13 +1,22 @@
 #include <stdint.h>
 #include <stm32f10x.h>
+//#include "main.h"
+
+void delay(void)
+{
+	for(volatile uint32_t d=0; d < 1E6; d++);
+}
 
 int main()
 {
+	*((uint32_t*)0x40021018)|= (uint32_t)0x40; //RCCBASE
+	
 	GPIOE->CRH |= GPIO_CRH_MODE14_1;
 	GPIOE->CRH |= GPIO_CRH_CNF14_0;
 	
-	for()
+	for(;;)
 	{
-		
+		GPIOE->ODR ^= GPIO_ODR_ODR14;
+		delay();
 	}
 }
