c9d705747d1f08bbc5beec1ab26a3445660764f5
dawidjar97 <mr.dj.xd@gmail.com>
2021-11-19 10:00:06 +0100

    lab3

---
+++ Lab3/arm_cube_01/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'arm_cube_01' 
+ * Target:  'arm_cube_01' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
