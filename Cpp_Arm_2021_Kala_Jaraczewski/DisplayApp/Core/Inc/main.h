e70caf6f2c1cc3dc15d78adce1b502f9ace55f6c
Dominik Kala <dominikkala@gmail.com>
2021-12-03 22:01:52 +0100

    feat: config update

--- Cpp_Arm_2021_Kala_Jaraczewski/DisplayApp/Core/Inc/main.h
+++ Cpp_Arm_2021_Kala_Jaraczewski/DisplayApp/Core/Inc/main.h
@@ -29,6 +29,17 @@ extern "C" {
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f1xx_hal.h"
 
+#include "stm32f1xx_ll_spi.h"
+#include "stm32f1xx_ll_system.h"
+#include "stm32f1xx_ll_gpio.h"
+#include "stm32f1xx_ll_exti.h"
+#include "stm32f1xx_ll_bus.h"
+#include "stm32f1xx_ll_cortex.h"
+#include "stm32f1xx_ll_rcc.h"
+#include "stm32f1xx_ll_utils.h"
+#include "stm32f1xx_ll_pwr.h"
+#include "stm32f1xx_ll_dma.h"
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -57,21 +68,31 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define B1_Pin GPIO_PIN_13
+#define B1_Pin LL_GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
 #define B1_EXTI_IRQn EXTI15_10_IRQn
-#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_Pin LL_GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
-#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_Pin LL_GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
-#define LD2_Pin GPIO_PIN_5
+#define LD2_Pin LL_GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
-#define TMS_Pin GPIO_PIN_13
+#define OLED_RES_Pin LL_GPIO_PIN_10
+#define OLED_RES_GPIO_Port GPIOB
+#define OLED_SCK_Pin LL_GPIO_PIN_13
+#define OLED_SCK_GPIO_Port GPIOB
+#define OLED_MOSI_Pin LL_GPIO_PIN_15
+#define OLED_MOSI_GPIO_Port GPIOB
+#define TMS_Pin LL_GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
-#define TCK_Pin GPIO_PIN_14
+#define TCK_Pin LL_GPIO_PIN_14
 #define TCK_GPIO_Port GPIOA
-#define SWO_Pin GPIO_PIN_3
+#define SWO_Pin LL_GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
+#define OLED_DC_Pin LL_GPIO_PIN_4
+#define OLED_DC_GPIO_Port GPIOB
+#define OLED_CS_Pin LL_GPIO_PIN_5
+#define OLED_CS_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
