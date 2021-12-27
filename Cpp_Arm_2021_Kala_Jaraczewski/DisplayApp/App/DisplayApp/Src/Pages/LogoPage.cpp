027a1c17de51f17b3c138f7e083fad5ad0fcae9d
Dominik Kala <dominikkala@gmail.com>
2021-12-28 00:13:42 +0100

    feat: removed old files

--- Cpp_Arm_2021_Kala_Jaraczewski/DisplayApp/App/DisplayApp/Src/Pages/LogoPage.cpp
+++ Cpp_Arm_2021_Kala_Jaraczewski/DisplayApp/App/DisplayApp/Src/Pages/LogoPage.cpp
@@ -3,7 +3,7 @@
 /// @note Copyright (c) 2021 ArmCpp - Kala, Jaraczewski
 
 #include "LogoPage.hpp"
-#include "../MonochromeGraphicLogoPolSl.hpp"
+#include "MonochromeGraphicLogoPolSl.hpp"
 
 LogoPage::LogoPage(MonochromeView::DynamicView& rView) :
     m_rView(rView)
