5ed348a22b003207b768a358046ef582087b9d75
Dawid Jaraczewski <mr.dj.xd@gmail.com>
2021-12-27 21:40:27 +0100

    feat: WriteChar impl

--- Cpp_Arm_2021_Kala_Jaraczewski/DisplayApp/App/MonochromeText/Src/MonochromeText.cpp
+++ Cpp_Arm_2021_Kala_Jaraczewski/DisplayApp/App/MonochromeText/Src/MonochromeText.cpp
@@ -13,7 +13,22 @@ void MonochromeText::WriteChar(MonochromeView::DynamicView& rView,
                                const char character, 
                                const uint8_t drawOption)
 {
-    // TODO: Add implementation.
+    // Check if character is valid
+    if (character < 32 || character > 126)
+    {
+        
+        // Check remaining space on current line
+        if (rView.Width() < (x + rFont.GetWidth()) ||
+            rView.Height() < (y + rFont.GetHeight()))
+        {
+            // Not enough space on current line
+        }
+        else
+        {
+            MonochromeView::ConstView font = rFont.GetCharView(character);
+            rView.DrawAt(x, y, font, drawOption);
+        }
+    }
 }
 
 void MonochromeText::WriteString(MonochromeView::DynamicView& rView, 
@@ -22,7 +37,13 @@ void MonochromeText::WriteString(MonochromeView::DynamicView& rView,
                                  const char* const pString, 
                                  const uint8_t drawOption)
 {
-    // TODO: Add implementation.
+    // TODO: Add x,y operations
+    uint8_t i = 0;
+	while(*(pString + i))
+    {
+        WriteChar(rView, x, y, rFont, *(pString + i), drawOption);
+		++i;
+    }
 }
 
 }
